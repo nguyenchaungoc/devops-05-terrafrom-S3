84f011ad12a61520134cc3fd49d7479cc4aa45d3
nguyenchaungoc <64840783+nguyenchaungoc@users.noreply.github.com>
2022-07-05 09:50:22 +0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,23 @@
+# khai báo provider, xác thực
+# https://registry.terraform.io/providers/hashicorp/aws/latest
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.20.1"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-southeast-1" # Singapore region
+}
+
+# tạo s3 bucket
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "devops-techmaster-05-ngocnct-01"
+  acl    = "private"
+}
